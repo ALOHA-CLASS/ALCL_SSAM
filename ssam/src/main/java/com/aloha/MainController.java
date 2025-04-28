6379185fbd742520fbbd6ad7378ae129f7d3415b
wwwalohacampus <h850415@naver.com>
2025-04-28 10:53:51 +0900

    ssam.mp3 파일없음 버그해결

--- ssam/src/main/java/com/aloha/MainController.java
+++ ssam/src/main/java/com/aloha/MainController.java
@@ -9,9 +9,9 @@ public class MainController {
     @FXML
     void initialize() {
         System.out.println("메인 화면 초기화");
-        String audio = "ssam.mp3";
-        Media media = new Media(getClass().getResource(audio).toExternalForm());
-        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        // String audio = "ssam.mp3";
+        // Media media = new Media(getClass().getResource(audio).toExternalForm());
+        // MediaPlayer mediaPlayer = new MediaPlayer(media);
         // mediaPlayer.play();
 
         // WebSocket 클라이언트 시작
