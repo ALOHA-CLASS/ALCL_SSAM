ac796cd4da8d1917677ba999ad0a9848a2205d15
wwwalohacampus <h850415@naver.com>
2025-04-29 09:52:33 +0900

    javafx.media 모듈 추가

--- deploy/install.bat
+++ deploy/install.bat
@@ -9,11 +9,10 @@ jpackage ^
   --name ssam_server ^
   --main-jar App2.jar ^
   --main-class com.aloha.Main ^
-  --java-options "--module-path app/lib/javafx-sdk-23.0.2/lib --add-modules javafx.controls,javafx.fxml -Xmx512m -Djava.awt.headless=false" ^
+  --java-options "--module-path app/lib/javafx-sdk-23.0.2/lib --add-modules javafx.controls,javafx.fxml,javafx.media -Xmx512m -Djava.awt.headless=false" ^
   --icon icon_server.ico ^
   --win-dir-chooser ^
   --win-menu ^
-  --win-console ^
   --win-shortcut
 
 echo [INFO] Installer build complete.
--- deploy/run.bat
+++ deploy/run.bat
@@ -5,7 +5,7 @@ set FX_LIB=lib\javafx-sdk-23.0.2\lib
 
 REM JavaFX 애플리케이션 실행
 start java --module-path "%FX_LIB%" ^
-     --add-modules javafx.controls,javafx.fxml ^
+     --add-modules javafx.controls,javafx.fxml,javafx.media ^
      -jar App2.jar
 
 exit
